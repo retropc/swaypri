6e3bb3aedcc764c9e9d26457182c7113fac848b4
Chris Porter <chris@warp13.co.uk>
2023-01-12 21:35:27 +0000

    add dynamic throttle based on time process has been alive

--- proc.py
+++ proc.py
@@ -6,20 +6,28 @@ def lookup_exe(pid):
   except (PermissionError, FileNotFoundError):
     return None
 
-def lookup_pid_from_exe(exe):
+def iterate_pids():
   for pid in os.listdir("/proc"):
     try:
       pid_v = int(pid)
-      if os.readlink("/proc/%s/exe" % pid) == exe:
-        yield pid_v
-    except (ValueError, PermissionError, FileNotFoundError):
+      yield pid_v
+    except ValueError:
+      continue
+
+def lookup_pid_from_exe(exe):
+  for pid in iterate_pids():
+    try:
+      path = os.readlink("/proc/%d/exe" % pid)
+      if path == exe:
+        yield pid
+    except (PermissionError, FileNotFoundError):
       continue
 
 def lookup_pid_from_uid(uid):
-  for pid in os.listdir("/proc"):
+  for pid in iterate_pids():
     try:
-      pid_v = int(pid)
-      if os.stat("/proc/%s" % pid).st_uid == uid:
-        yield pid_v
-    except (ValueError, PermissionError, FileNotFoundError):
+      s = os.stat("/proc/%d" % pid)
+      if s.st_uid == uid:
+        yield pid, s
+    except (PermissionError, FileNotFoundError):
       continue
